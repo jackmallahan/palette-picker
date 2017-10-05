b795afe2bb19eb4e8d5fa4c48ca72fa8f20562eb
Jack Mallahan <jackmallahan@gmail.com>
2017-10-05 15:46:50 -0600

    posts project to db and appends project

--- knexfile.js
+++ knexfile.js
@@ -1,50 +1,47 @@
 // Update with your config settings.
 
 module.exports = {
-
-  development: {
+	development: {
 		client: 'pg',
 		connection: 'postgres://localhost/palette_picker',
 		migrations: {
 			directory: './db/migrations'
 		},
-    seeds: {
-      directory: './db/seeds/dev'
-    },
+		seeds: {
+			directory: './db/seeds/dev'
+		},
 		useNullAsDefault: true
 	},
 
+	staging: {
+		client: 'postgresql',
+		connection: {
+			database: 'my_db',
+			user: 'username',
+			password: 'password'
+		},
+		pool: {
+			min: 2,
+			max: 10
+		},
+		migrations: {
+			tableName: 'knex_migrations'
+		}
+	},
 
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
+	production: {
+		client: 'postgresql',
+		connection: {
+			database: 'my_db',
+			user: 'username',
+			password: 'password'
+		},
+		pool: {
+			min: 2,
+			max: 10
+		},
+		migrations: {
+			tableName: 'knex_migrations'
+		}
+	}
 };
--- server.js
+++ server.js
@@ -1,46 +1,42 @@
-const express = require('express')
+const express = require('express');
 const app = express();
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 const path = require('path');
 
 const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
 const database = require('knex')(configuration);
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 app.set('port', process.env.PORT || 3000);
 
 app.get('/api/v1/projects', (request, response) => {
-  database('projects').select()
-    .then(project => response.status(200).json(project))
-    .catch(error => response.status(500).json({error}));
+	database('projects')
+		.select()
+		.then(project => response.status(200).json(project))
+		.catch(error => response.status(500).json({ error }));
 });
 
 app.post('/api/v1/projects', (request, response) => {
-  const name = request.body;
-  console.log(name)
+	const name = request.body;
 
-  for (let requiredParameter of ['name']) {
-    if (!name[requiredParameter]) {
-      return response
-        .status(422)
-        .send({ error: `You must include a name for your project` });
-    }
-  }
+	if (!name.name) {
+		return response.status(422).send({ error: `You must include a name for your project` });
+	}
 
-  database('projects').insert(name, 'id')
-    .then(name => {
-      response.status(201).json({ id: name[0] })
-    })
-    .catch(error => {
-      response.status(500).json({ error });
-    });
-})
+	database('projects')
+		.insert(name, '*')
+		.then(projectObj => {
+			console.log('projectObj', projectObj);
+			return response.status(201).json(projectObj);
+		})
+		.catch(error => response.status(500).json({ error }));
+});
 
 app.listen(app.get('port'), () => {
-  console.log(`Palette Picker is running on ${app.get('port')}.`);
+	console.log(`Palette Picker is running on ${app.get('port')}.`);
 });
